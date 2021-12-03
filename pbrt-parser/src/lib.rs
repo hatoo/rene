24b1107a6a9fb35fcc8814179c59120657ddb62c
hatoo <hato2000@gmail.com>
2021-12-03 23:54:33 +0900

    rgb

--- pbrt-parser/src/lib.rs
+++ pbrt-parser/src/lib.rs
@@ -3,8 +3,8 @@ use nom::{
     branch::alt,
     bytes::complete::{escaped, tag, take_while, take_while1},
     character::complete::{alphanumeric1, char, one_of},
-    combinator::{cut, value},
-    multi::{many0, many1},
+    combinator::{cut, map, map_res, opt, value},
+    multi::many0,
     number::complete::float,
     sequence::{preceded, terminated},
     AsChar, IResult,
@@ -21,10 +21,11 @@ struct LookAt {
     up: Vec3,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 enum Value {
     Float(f32),
     Integer(i32),
+    Vec3(Vec3),
 }
 
 struct Argument<'a> {
@@ -35,6 +36,7 @@ struct Argument<'a> {
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 enum SceneObjectType {
     Camera,
+    LightSource,
 }
 
 struct SceneObject<'a> {
@@ -73,30 +75,47 @@ fn parse_str(i: &str) -> IResult<&str, &str> {
     preceded(char('\"'), cut(terminated(parse, char('\"'))))(i)
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 enum ArgumentType {
     Float,
+    Rgb,
 }
 
 fn parse_argument_type(input: &str) -> IResult<&str, ArgumentType> {
-    value(ArgumentType::Float, tag("float"))(input)
+    alt((
+        value(ArgumentType::Float, tag("float")),
+        value(ArgumentType::Rgb, tag("rgb")),
+    ))(input)
 }
 
-fn single_bracket<'a, T: Clone, F: Fn(&'a str) -> IResult<&'a str, T>>(
+fn bracket_single<'a, T: Clone, F: Fn(&'a str) -> IResult<&'a str, T>>(
     p: F,
     input: &'a str,
 ) -> IResult<&'a str, T> {
     let (rest, _) = char('[')(input)?;
     let (rest, t) = preceded(sp, p)(rest)?;
-    let (rest, _) = value((), preceded(sp, char(',')))(rest).unwrap_or((rest, ()));
     value(t, preceded(sp, char(']')))(rest)
+}
+
+fn bracket_triple<'a, T: Clone, F: Fn(&'a str) -> IResult<&'a str, T>>(
+    p: &F,
+    input: &'a str,
+) -> IResult<&'a str, [T; 3]> {
+    let (rest, _) = char('[')(input)?;
+    let (rest, x1) = preceded(sp, p)(rest)?;
+    let (rest, x2) = preceded(sp, p)(rest)?;
+    let (rest, x3) = preceded(sp, p)(rest)?;
+    value([x1, x2, x3], preceded(sp, char(']')))(rest)
 }
 
 impl ArgumentType {
     fn parse_value(self, input: &str) -> IResult<&str, Value> {
         match self {
-            ArgumentType::Float => alt((float, |i| single_bracket(float, i)))(input)
+            ArgumentType::Float => alt((float, |i| bracket_single(float, i)))(input)
                 .map(|(rest, f)| (rest, Value::Float(f))),
+            ArgumentType::Rgb => {
+                bracket_triple(&float, input).map(|(rest, v)| (rest, Value::Vec3(v.into())))
+            }
         }
     }
 }
@@ -105,7 +124,7 @@ fn parse_argument_type_name(input: &str) -> IResult<&str, (ArgumentType, &str)> 
     fn parse(input: &str) -> IResult<&str, (ArgumentType, &str)> {
         let (rest, ty) = parse_argument_type(input)?;
         let (rest, _) = char(' ')(rest)?;
-        let (rest, ident): _ = take_while(|c: char| c.is_alphanum())(rest)?;
+        let (rest, ident) = take_while(|c: char| c.is_alphanum())(rest)?;
         Ok((rest, (ty, ident)))
     }
     preceded(char('\"'), cut(terminated(parse, char('\"'))))(input)
@@ -113,7 +132,9 @@ fn parse_argument_type_name(input: &str) -> IResult<&str, (ArgumentType, &str)> 
 
 fn parse_argument(input: &str) -> IResult<&str, Argument> {
     let (rest, (ty, name)) = parse_argument_type_name(input)?;
+    dbg!(ty, name);
     let (rest, value) = preceded(sp, |i| ty.parse_value(i))(rest)?;
+    dbg!(ty, value);
 
     Ok((rest, Argument { name, value }))
 }
@@ -128,7 +149,10 @@ fn parse_look_at(input: &str) -> IResult<&str, LookAt> {
 }
 
 fn parse_scene_object_type(input: &str) -> IResult<&str, SceneObjectType> {
-    value(SceneObjectType::Camera, tag("Camera"))(input)
+    alt((
+        value(SceneObjectType::Camera, tag("Camera")),
+        value(SceneObjectType::LightSource, tag("LightSource")),
+    ))(input)
 }
 
 fn parse_scene_object(input: &str) -> IResult<&str, SceneObject> {
@@ -185,11 +209,17 @@ mod test {
     }
 
     #[test]
+    fn test_bracket_triple() {
+        let (rest, v3) = bracket_triple(&float, "[.4 .45 .5]").unwrap();
+        assert_eq!(rest, "");
+        assert!(abs_diff_eq!(v3[..], [0.4, 0.45, 0.5]));
+    }
+
+    #[test]
     fn test_parse_scene_object() {
         for q in [
             r#"Camera "perspective" "float fov" 45"#,
             r#"Camera "perspective" "float fov" [45]"#,
-            r#"Camera "perspective" "float fov" [45,]"#,
         ] {
             let (rest, camera) = parse_scene_object(q).unwrap();
             assert_eq!(rest, "");
@@ -202,6 +232,20 @@ mod test {
                 Value::Float(f) => assert!(abs_diff_eq!(f, 45.0)),
                 _ => panic!(),
             }
+        }
+
+        dbg!("here");
+        let (rest, light_source) =
+            parse_scene_object(r#"LightSource "infinite" "rgb L" [.4 .45 .5]"#).unwrap();
+        assert_eq!(rest, "");
+
+        assert_eq!(light_source.object_type, SceneObjectType::LightSource);
+        assert_eq!(light_source.t, "infinite");
+        assert_eq!(light_source.arguments.len(), 1);
+        assert_eq!(light_source.arguments[0].name, "L");
+        match light_source.arguments[0].value {
+            Value::Vec3(v3) => assert!(abs_diff_eq!(v3.to_array()[..], [0.4, 0.45, 0.5])),
+            _ => panic!(),
         }
     }
 }
