5a2a959834fd1d8fcf6b32323a2ad04e3c191509
hatoo <hato2000@gmail.com>
2021-12-31 14:15:33 +0900

    Add intermediate layer

--- rene/src/scene.rs
+++ rene/src/scene.rs
@@ -1,6 +1,13 @@
-use glam::{vec3a, Affine3A};
+use glam::{vec3, Affine3A};
 use rene_shader::{material::EnumMaterial, LookAt, Uniform};
 use thiserror::Error;
+
+use self::intermediate_scene::{
+    Camera, Infinite, IntermediateScene, IntermediateWorld, LightSource, Material, Matte,
+    SceneObject, Shape, Sphere, WorldObject,
+};
+
+mod intermediate_scene;
 
 #[derive(Debug)]
 pub struct TlasInstance {
@@ -18,13 +25,9 @@ pub struct Scene {
 }
 
 #[derive(Error, Debug)]
-pub enum CreateSceneError<'a> {
-    #[error("Invalid Camera type")]
-    InvalidCamera(&'a str),
-    #[error("Invalid LightSource type")]
-    InvalidLightSource(&'a str),
-    #[error("Invalid Material type")]
-    InvalidMaterial(&'a str),
+pub enum CreateSceneError {
+    #[error("Failed to convert to intermediate type")]
+    IntermediateError(#[from] intermediate_scene::Error),
     #[error("No Material")]
     NoMaterial,
 }
@@ -36,28 +39,21 @@ struct WorldState {
 }
 
 impl Scene {
-    pub fn create<'a>(
-        scene_description: &[pbrt_parser::Scene<'a>],
-    ) -> Result<Self, CreateSceneError<'a>> {
+    pub fn create(scene_description: &[pbrt_parser::Scene]) -> Result<Self, CreateSceneError> {
         let mut scene = Self::default();
         for desc in scene_description {
-            match desc {
-                pbrt_parser::Scene::LookAt(pbrt_parser::LookAt { eye, look_at, up }) => {
-                    scene.uniform.look_at = LookAt {
-                        eye: *eye,
-                        look_at: *look_at,
-                        up: *up,
-                    };
+            match IntermediateScene::from_scene(desc)? {
+                IntermediateScene::LookAt(intermediate_scene::LookAt { eye, look_at, up }) => {
+                    scene.uniform.look_at = LookAt { eye, look_at, up };
                 }
-                pbrt_parser::Scene::SceneObject(obj) => match obj.object_type {
-                    pbrt_parser::SceneObjectType::Camera => {
-                        if obj.t != "perspective" {
-                            return Err(CreateSceneError::InvalidCamera(obj.t));
+                IntermediateScene::SceneObject(obj) => match obj {
+                    SceneObject::Camera(camera) => match camera {
+                        Camera::Perspective(p) => {
+                            scene.uniform.camera.fov = p.fov;
                         }
-                        scene.uniform.camera.fov = obj.get_float("fov").unwrap_or(90.0);
-                    }
+                    },
                 },
-                pbrt_parser::Scene::World(worlds) => {
+                IntermediateScene::World(worlds) => {
                     scene.append_world(Default::default(), &worlds)?;
                 }
             }
@@ -65,43 +61,42 @@ impl Scene {
         Ok(scene)
     }
 
-    fn append_world<'a>(
+    fn append_world(
         &mut self,
         mut state: WorldState,
-        worlds: &[pbrt_parser::World<'a>],
-    ) -> Result<(), CreateSceneError<'a>> {
+        worlds: &[IntermediateWorld],
+    ) -> Result<(), CreateSceneError> {
         for w in worlds {
             match w {
-                pbrt_parser::World::Attribute(worlds) => {
+                IntermediateWorld::Attribute(worlds) => {
                     self.append_world(state, worlds.as_slice())?
                 }
-                pbrt_parser::World::Translate(v) => {
-                    state.current_matrix.w_axis += *v;
+                IntermediateWorld::Matrix(m) => {
+                    state.current_matrix = state.current_matrix * *m;
                 }
-                pbrt_parser::World::WorldObject(obj) => match obj.object_type {
-                    pbrt_parser::WorldObjectType::LightSource => {
-                        if obj.t != "infinite" {
-                            return Err(CreateSceneError::InvalidLightSource(obj.t));
+                IntermediateWorld::WorldObject(obj) => match obj {
+                    WorldObject::LightSource(lightsource) => match lightsource {
+                        LightSource::Infinite(Infinite { color }) => {
+                            self.uniform.background += *color;
                         }
-                        self.uniform.background += obj.get_rgb("L").unwrap_or(vec3a(1.0, 1.0, 1.0));
-                    }
-                    pbrt_parser::WorldObjectType::Material => {
-                        if obj.t != "matte" {
-                            return Err(CreateSceneError::InvalidMaterial(obj.t));
+                    },
+                    WorldObject::Material(material) => match material {
+                        Material::Matte(Matte { color }) => {
+                            state.current_material_index = Some(self.materials.len());
+                            self.materials.push(EnumMaterial::new_lambertian(*color));
                         }
-                        state.current_material_index = Some(self.materials.len());
-                        self.materials.push(EnumMaterial::new_lambertian(
-                            obj.get_rgb("Kd").unwrap_or(vec3a(0.5, 0.5, 0.5)),
-                        ));
-                    }
-                    pbrt_parser::WorldObjectType::Shape => self.tlas.push(TlasInstance {
-                        shader_offset: 0,
-                        matrix: state.current_matrix,
-                        material_index: state
-                            .current_material_index
-                            .ok_or(CreateSceneError::NoMaterial)?,
-                        blas_index: None,
-                    }),
+                    },
+                    WorldObject::Shape(shape) => match shape {
+                        Shape::Sphere(Sphere { radius }) => self.tlas.push(TlasInstance {
+                            shader_offset: 0,
+                            matrix: state.current_matrix
+                                * Affine3A::from_scale(vec3(*radius, *radius, *radius)),
+                            material_index: state
+                                .current_material_index
+                                .ok_or(CreateSceneError::NoMaterial)?,
+                            blas_index: None,
+                        }),
+                    },
                 },
             }
         }
---
+++ rene/src/scene/intermediate_scene.rs
@@ -0,0 +1,144 @@
+use glam::{vec3a, Affine3A, Vec3A};
+use rene_shader::camera::PerspectiveCamera;
+use thiserror::Error;
+
+#[derive(PartialEq, Debug)]
+pub struct LookAt {
+    pub eye: Vec3A,
+    pub look_at: Vec3A,
+    pub up: Vec3A,
+}
+
+pub enum SceneObject {
+    Camera(Camera),
+}
+
+pub enum Camera {
+    Perspective(PerspectiveCamera),
+}
+
+pub enum IntermediateWorld {
+    Attribute(Vec<IntermediateWorld>),
+    WorldObject(WorldObject),
+    Matrix(Affine3A),
+}
+
+pub enum WorldObject {
+    LightSource(LightSource),
+    Material(Material),
+    Shape(Shape),
+}
+
+pub enum LightSource {
+    Infinite(Infinite),
+}
+
+pub struct Infinite {
+    pub color: Vec3A,
+}
+
+pub enum Material {
+    Matte(Matte),
+}
+
+pub struct Matte {
+    pub color: Vec3A,
+}
+
+pub enum Shape {
+    Sphere(Sphere),
+}
+
+pub struct Sphere {
+    pub radius: f32,
+}
+
+pub enum IntermediateScene {
+    LookAt(LookAt),
+    SceneObject(SceneObject),
+    World(Vec<IntermediateWorld>),
+}
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("Invalid Camera type")]
+    InvalidCamera(String),
+    #[error("Invalid LightSource type")]
+    InvalidLightSource(String),
+    #[error("Invalid Material type")]
+    InvalidMaterial(String),
+    #[error("Invalid Shape type")]
+    InvalidShape(String),
+}
+
+impl IntermediateWorld {
+    fn from_world(world: &pbrt_parser::World) -> Result<Self, Error> {
+        match world {
+            pbrt_parser::World::WorldObject(obj) => match obj.object_type {
+                pbrt_parser::WorldObjectType::LightSource => match obj.t {
+                    "infinite" => {
+                        let color = obj.get_rgb("L").unwrap_or(vec3a(1.0, 1.0, 1.0));
+                        Ok(Self::WorldObject(WorldObject::LightSource(
+                            LightSource::Infinite(Infinite { color }),
+                        )))
+                    }
+                    t => Err(Error::InvalidLightSource(t.to_string())),
+                },
+                pbrt_parser::WorldObjectType::Material => match obj.t {
+                    "matte" => {
+                        let color = obj.get_rgb("Kd").unwrap_or(vec3a(0.5, 0.5, 0.5));
+                        Ok(Self::WorldObject(WorldObject::Material(Material::Matte(
+                            Matte { color },
+                        ))))
+                    }
+                    t => Err(Error::InvalidMaterial(t.to_string())),
+                },
+                pbrt_parser::WorldObjectType::Shape => match obj.t {
+                    "sphere" => {
+                        let radius = obj.get_float("radius").unwrap_or(1.0);
+                        Ok(Self::WorldObject(WorldObject::Shape(Shape::Sphere(
+                            Sphere { radius },
+                        ))))
+                    }
+                    t => Err(Error::InvalidShape(t.to_string())),
+                },
+            },
+            pbrt_parser::World::Attribute(worlds) => worlds
+                .into_iter()
+                .map(Self::from_world)
+                .collect::<Result<Vec<Self>, Error>>()
+                .map(IntermediateWorld::Attribute),
+            pbrt_parser::World::Translate(translation) => Ok(Self::Matrix(
+                Affine3A::from_translation((*translation).into()),
+            )),
+        }
+    }
+}
+
+impl IntermediateScene {
+    pub fn from_scene(scene: &pbrt_parser::Scene) -> Result<Self, Error> {
+        match scene {
+            pbrt_parser::Scene::LookAt(look_at) => Ok(Self::LookAt(LookAt {
+                eye: look_at.eye,
+                look_at: look_at.look_at,
+                up: look_at.up,
+            })),
+            pbrt_parser::Scene::SceneObject(obj) => match obj.object_type {
+                pbrt_parser::SceneObjectType::Camera => match obj.t {
+                    "perspective" => {
+                        let fov = obj.get_float("fov").unwrap_or(90.0);
+                        Ok(Self::SceneObject(SceneObject::Camera(Camera::Perspective(
+                            PerspectiveCamera { fov },
+                        ))))
+                    }
+                    t => Err(Error::InvalidCamera(t.to_string())),
+                },
+            },
+            pbrt_parser::Scene::World(worlds) => worlds
+                .into_iter()
+                .map(IntermediateWorld::from_world)
+                .collect::<Result<Vec<IntermediateWorld>, _>>()
+                .map(IntermediateScene::World),
+        }
+    }
+}
