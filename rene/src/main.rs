bef097292bf5fdb21ad9e490ecea99df0e0ff879
hatoo <hato2000@gmail.com>
2021-12-31 12:58:10 +0900

    Update ash to 0.35

--- rene/src/main.rs
+++ rene/src/main.rs
@@ -62,7 +62,7 @@ fn main() {
         .map(|c_str| c_str.as_ptr())
         .collect();
 
-    let entry = ash::Entry::new();
+    let entry = unsafe { ash::Entry::load() }.unwrap();
 
     assert_eq!(
         check_validation_layer_support(
