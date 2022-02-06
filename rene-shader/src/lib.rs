98c7a80d39042e66261d9ef9912f75256ab8d1c5
hatoo <hato2000@gmail.com>
2022-02-06 23:44:34 +0900

    Revert "Fix(?)"
    This reverts commit bfb154d5f4fdc22c72b519c8844f35ed44da001f.

--- rene-shader/src/lib.rs
+++ rene-shader/src/lib.rs
@@ -311,9 +311,11 @@ pub fn main_ray_generation(
                 color *= f * normal.dot(wi).abs();
                 let pdf = 0.5 * pdf + 0.5 * weight * payload_pdf.pdf;
 
-                if pdf > 1e-5 {
-                    color /= pdf;
+                if pdf < 1e-5 {
+                    break;
                 }
+
+                color /= pdf;
             } else {
                 let sampled_f = bsdf.sample_f(wo, &mut rng);
 
