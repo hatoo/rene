db4786b18ad2b063389bf83436d478b80cf8594e
hatoo <hato2000@gmail.com>
2022-02-16 23:36:27 +0900

    std::env::set_var("NO_SPIRV_OPT", "1");

--- rene/build.rs
+++ rene/build.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use spirv_builder::{Capability, MetadataPrintout, SpirvBuilder};
 
 fn main() -> Result<(), Box<dyn Error>> {
+    std::env::set_var("NO_SPIRV_OPT", "1");
     SpirvBuilder::new("../rene-shader", "spirv-unknown-spv1.3")
         .capability(Capability::RayTracingKHR)
         .capability(Capability::RuntimeDescriptorArray)
